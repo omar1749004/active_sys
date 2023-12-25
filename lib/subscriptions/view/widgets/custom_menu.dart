daa60755feacbcc70708761758fa977d946f77e7
omar1749004 <mhsnmr68@gmail.com>
2023-12-25 22:13:18 +0200

    handl error

--- lib/subscriptions/view/widgets/custom_menu.dart
+++ lib/subscriptions/view/widgets/custom_menu.dart
@@ -1,5 +1,6 @@
 import 'package:active_system/core/constant/menu_items.dart';
-import 'package:active_system/subscriptions/view/widgets/custom_menu_button.dart';
+import 'package:active_system/subscriptions/view/widgets/custom_button.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
