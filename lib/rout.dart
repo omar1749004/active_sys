60a269e69a3e53e522c3420edd954079d437e39a
omar1749004 <mhsnmr68@gmail.com>
2023-12-27 10:46:04 +0200

    add mange users page

--- lib/rout.dart
+++ lib/rout.dart
@@ -2,6 +2,7 @@ import 'package:active_system/core/constant/app_route.dart';
 import 'package:active_system/core/middleware/mymiddleware.dart';
 import 'package:active_system/home/view/screen/home.dart';
 import 'package:active_system/subscriptions/view/subscriptions_view.dart';
+import 'package:active_system/users/view/screen/users_view.dart';
 import 'package:get/get.dart';
 
 List<GetPage<dynamic>> route = [
@@ -12,5 +13,9 @@ List<GetPage<dynamic>> route = [
   GetPage(
       name: AppRoute.subscriptionid,
       page: () => const SubscriptionsView(),
+      middlewares: [MyMiddleWare()]),
+      GetPage(
+      name: AppRoute.usersid,
+      page: () => const UsersView(),
       middlewares: [MyMiddleWare()]),
 ];
