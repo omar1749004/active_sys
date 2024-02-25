ac4ef4c0283bf5f659eaebbaa3e8ccd91911f5b8
omar1749004 <mhsnmr68@gmail.com>
2024-02-25 18:54:28 +0200

    edit

--- lib/controller/expenses_controller.dart
+++ lib/controller/expenses_controller.dart
@@ -275,35 +275,3 @@ class ExpensesControllerImp extends ExpensesController {
     update();
   }
 }
-
-// import 'package:active_system/data/service/remote/expenses_data.dart';
-
-// var res = await ExpensesData().add(
-//   {
-//     "reason": text,
-//     "value":  text,
-//     "adminId": ,
-//   },
-// );
-
-
-// var res = await ExpensesData().delete(
-//   {
-//    "id": id
-//   }
-
-// );
-
-// var res = await ExpensesData().dateSearch(
-//   {
-//    "start_date": id,
-//    "end_date": id,
-//   }
-
-// );
-// var res = await ExpensesData().search(
-//   {
-//    "search": id,
-//   }
-
-// );
